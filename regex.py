fff0946eb7ebe7424c63fc970e50762f01c95e6f
AnnyA95 <anni032@hotmail.com>
2014-10-29 16:37:54 +0100

    pages veranderd
    ik heb even meerder pagina's toegevoegd om te kijken of hij nog dubbel
    doet

--- regex.py
+++ regex.py
@@ -7,5 +7,6 @@ strs = "http://michelle.initworks.com/TestCrawler/items/item1.php"
 match = re.search(r'\bitems\b',strs)
 if re.search(r'\bitems\b',strs):
     print "Found"
+
 else:
     print "Not Found"
