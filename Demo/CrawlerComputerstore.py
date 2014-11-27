ae5aa050bedcec52a05e29d97939e5324779d126
Michelleritzema <michelleritzema@hotmail.com>
2014-11-27 13:20:00 +0100

    crawler met prints

--- Demo/CrawlerComputerstore.py
+++ Demo/CrawlerComputerstore.py
@@ -33,7 +33,7 @@ def get_categories():
         for link in category_link:
             name = link.get('title').replace(" ", "")
             category_names.append(name)
-            # print name
+            print "name of categorie found: " + name
         container_sources = container.findChildren('a', {'class': 'image'})
         for container_source in container_sources:
             source = "http://www.computerstore.nl" + container_source.get('href')
@@ -50,7 +50,6 @@ def get_categories():
 def walk_through_categories(categories, category_names):
     a_index = 0
     for category in categories:
-        time.sleep(5)
         cat_name = category_names[a_index]
         max_amount = get_pages(category)
         if not(cat_name == "Barebones" or cat_name == "UpgradeKits"):
@@ -63,6 +62,7 @@ def walk_through_categories(categories, category_names):
 # element wordt eruit gehaald en opgeslagen onder de variabele 'max-amount'. Deze
 # waarde wordt terug gegeven aan de functie walk_through_categories().
 def get_pages(url):
+    time.sleep(3)
     source_code = requests.get(url)
     plain_text = source_code.text
     soup = BeautifulSoup(plain_text)
@@ -112,21 +112,31 @@ def update_node(name, date):
 def get_info_per_page(url, cat_name, max_pages):
     page = 1
     while page <= max_pages:
-        time.sleep(10)
+        print "---------------------------------------"
+        print "   Starting at a new page of items"
+        print "---------------------------------------"
+        print ""
+        time.sleep(5)
         url = url + "?sort=popularity&dir=d&page=" + str(page) + "&items=12"
         source_code = requests.get(url)
         plain_text = source_code.text
         soup = BeautifulSoup(plain_text)
+        print "test: made soup of items page"
         for item in soup.findAll('li', {'class': 'product-list-item'}):
+            print "test: found product-list-item"
             a_children = item.findChildren('a', {'class': 'product-list-item--image-link'})
             for a_child in a_children:
+                #print a_child
+                print "test: found product-list-item--image-link"
                 link_item = "http://www.computerstore.nl" + a_child.get('href')
                 print "cat_name: " + cat_name
                 print "link_item: " + link_item
                 try:
+                    print "test: inside try function, getting details"
                     get_details(link_item, cat_name)
                 except socket.error as error:
                     if error.errno == errno.WSAECONNRESET:
+                        print "there was an error, sleeping for 30 seconds and trying again..."
                         time.sleep(30)
                         get_info_per_page(url, cat_name, max_pages)
         page += 1
@@ -140,6 +150,10 @@ def get_info_per_page(url, cat_name, max_pages):
 # de 'source' variabele. Vervolgens wordt de functie check_for_nodes() aangeroepen om
 # te checken of de node zich al in de database bevindt.
 def get_details(link_item, cat_name):
+    print "-------------------------------------"
+    print "   Starting at a new detail page"
+    print "-------------------------------------"
+    print ""
     time.sleep(5)
     today = datetime.date.today()
     date = today.strftime("%d-%m-%Y")
@@ -147,31 +161,33 @@ def get_details(link_item, cat_name):
     source_code = requests.get(link_item)
     plain_text = source_code.text
     soup = BeautifulSoup(plain_text)
+    print "test: made a soup of the item detail page"
     #print soup
-    print "test1"
     for item in soup.findAll('div', {'class': 'product_container'}):
         #print item
-        print "test2"
+        print "test: found product_container"
         name_children = item.findChildren('span', {'itemprop': 'name'})
-        print "test3"
         if not (name_children == ""):
-            print "Name is found"
+            print "test: name is found"
             for name_child in name_children:
                 name_raw = name_child.text
                 name = name_raw.strip()
                 print "name: " + name
         else:
-            print "Name is empty, using default name."
+            print "test: name is empty, using default name."
             name = "unknown"
+            print "name: " + name
         img_children = item.findChildren('img', {'class': 'hasImageZoom'})
         if not (img_children == ""):
+            print "test: image is found"
             for img_child in img_children:
                 source = img_child.get('data-img-large')
                 print "source: " + source
         else:
             print "Img is empty, using default img."
-            source = "emptyimg"
-        check_for_nodes(cat_name, link_item, name, source, date)
+            source = "empty_img"
+            print "source: " + source
+        #check_for_nodes(cat_name, link_item, name, source, date)
 
 
 get_categories()
