86e40b868ddc4cdb8310ef91a8dff6297906e1ab
Michelleritzema <michelleritzema@hotmail.com>
2014-11-29 14:44:42 +0100

    Crawler v.1.1

--- Demo/CrawlerComputerstore.py
+++ Demo/CrawlerComputerstore.py
@@ -160,6 +160,111 @@ def get_info_per_page(url, cat_name, max_pages):
         page += 1
 
 
+def get_name(item_name, item_container):
+    if item_name == "product_container":
+        for name_children in item_container.findChildren('span', {'itemprop': 'name'}):
+            for name_child in name_children:
+                name = name_child.strip()
+    elif item_name == "product-page":
+        for name_child in item_container.findChildren('span', {'class': 'js-product-name'}):
+            name_raw = name_child.text
+            name = name_raw.strip()
+    else:
+        name = "unknown"
+    print "name: " + name
+    return name
+
+
+def get_price(item_name, item_container):
+    price = 0
+    if item_name == "product_container":
+        for price_child in item_container.findChildren('div', {'class': 'price'}):
+            price_raw = price_child.text
+    elif item_name == "product-page":
+        for price_child in item_container.findChildren('strong', {'class': 'sales-price--current'}):
+            price_raw = price_child.text
+    else:
+        price_raw = "0"
+    price_array = re.findall(r'\d{1,6}[\,.]{1}\d{0,2}', price_raw)
+    for price_item in price_array:
+        price_str = price_item.replace(',', '.')
+        price_float = float(price_str)
+        price = ('%.2f' % price_float)
+    print "price: " + price
+    return price
+
+
+def get_img(item_name, item_container):
+    if item_name == "product_container":
+        for img_child in item_container.findChildren('img', {'class': 'hasImageZoom'}):
+            source = img_child.get('data-img-large')
+    elif item_name == "product-page":
+        for img_child in item_container.findChildren('img', {'class': 'media-gallery--main-image'}):
+            source = str(img_child.get('src'))
+    else:
+        source = "unknown"
+    print "source: " + source
+    return source
+
+
+def get_spec_title(item_name, item_container):
+    spec_title = []
+    if item_name == "product_container":
+        for specs_title_child in item_container.findChildren('td', {'class': 'table_spectable_spec'}):
+            if specs_title_child.findChildren('span', {'class': 'table_spectable_spec_titletext'}):
+                for specs_title_raw in specs_title_child.findChildren('span',
+                                                                      {'class': 'table_spectable_spec_titletext'}):
+                    specs_title_txt = specs_title_raw.text.replace(" ", "").lstrip().rstrip()
+                    spec_title.append(re.sub('\s+', ' ', specs_title_txt))
+            else:
+                specs_title_txt = specs_title_child.text.replace(" ", "").lstrip().rstrip()
+                spec_title.append(re.sub('\s+', ' ', specs_title_txt))
+    elif item_name == "product-page":
+        for specs_title_child in item_container.findChildren('dt', {'class': 'product-specs--item-title'}):
+            if specs_title_child.findChildren('span', {'class': 'product-specs--help-title'}):
+                for specs_title_raw in specs_title_child.findChildren('span', {'class': 'product-specs--help-title'}):
+                    specs_title_txt = specs_title_raw.text.replace(" ", "").lstrip().rstrip()
+                    spec_title.append(re.sub('\s+', ' ', specs_title_txt))
+            else:
+                specs_title_txt = specs_title_child.text.replace(" ", "").lstrip().rstrip()
+                spec_title.append(re.sub('\s+', ' ', specs_title_txt))
+    print spec_title
+    return spec_title
+
+
+def get_spec_desc(item_name, item_container):
+    spec_desc = []
+    if item_name == "product_container":
+        for specs_desc_child in item_container.findChildren('td', {'class': 'table_spectable_specdescription'}):
+            specs_desc_txt = specs_desc_child.text.lstrip().rstrip()
+            spec_desc.append(re.sub('\s+', ' ', specs_desc_txt))
+    elif item_name == "product-page":
+        for specs_desc_child in item_container.findChildren('dd', {'class': 'product-specs--item-spec'}):
+            if specs_desc_child.findChildren('span', {'class': 'icon-fallback'}):
+                for specs_desc_raw in specs_desc_child.findChildren('span', {'class': 'icon-fallback'}):
+                    specs_desc_txt = specs_desc_raw.text.replace(" ", "").lstrip().rstrip()
+                    spec_desc.append(re.sub('\s+', ' ', specs_desc_txt))
+            else:
+                specs_desc_txt = specs_desc_child.text.replace(" ", "").lstrip().rstrip()
+                spec_desc.append(re.sub('\s+', ' ', specs_desc_txt))
+    print spec_desc
+    return spec_desc
+
+
+def get_manufacture_code(item_name, spec_title, spec_desc):
+    if spec_title.index('Fabrikantcode'):
+        index = spec_title.index('Fabrikantcode')
+        manufacture_code = spec_desc[index]
+    elif item_name == "product_container":
+        manufacture_code = spec_desc[0]
+    elif item_name == "product-page":
+        manufacture_code = spec_desc[3]
+    else:
+        manufacture_code = "unknown"
+    print "manufacture_code: " + manufacture_code
+    return manufacture_code
+
+
 # Deze functie haalt alle details op van elk product dat gevonden wordt. Er wordt gezocht
 # naar alle divs met de klas 'product_container'. Vervolgens wordt hierbinnen gezocht
 # naar alle spans met een itemprop die 'name' is. De naam wordt opgeslagen onder de
@@ -171,116 +276,31 @@ def get_details(link_item, cat_name):
     time.sleep(15)
     today = datetime.date.today()
     date = today.strftime("%d-%m-%Y")
-    print "Current date: " + date
+    #print "Current date: " + date
     source_code = requests.get(link_item)
     plain_text = source_code.text
     soup = BeautifulSoup(plain_text)
-    # print soup
-    spec_title = []
-    spec_desc = []
-    name = "unknown"
-    price = 0
-    source = "unknown"
-    for item in soup.findAll('div', {'class': 'product_container'}):
-        print "test: found product_container"
-        #print item
-        for name_children in item.findChildren('span', {'itemprop': 'name'}):
-            for name_child in name_children:
-                #print name_child
-                name = name_child.strip()
-                print "name: " + name
-        for price_child in item.findChildren('div', {'class': 'price'}):
-            #print price_child
-            price_raw = price_child.text
-            price_array = re.findall(r'\d{1,6}[\,.]{1}\d{0,2}', price_raw)
-            for price_item in price_array:
-                price_str = price_item.replace(',', '.')
-                price_float = float(price_str)
-                price = ('%.2f' % price_float)
-                print "price: " + price
-        for img_child in item.findChildren('img', {'class': 'hasImageZoom'}):
-            #print img_children
-            source = img_child.get('data-img-large')
-            print "source: " + source
-        for specs_title_child in item.findChildren('td', {'class': 'table_spectable_spec'}):
-            #print specs_title_child
-            if specs_title_child.findChildren('span', {'class': 'table_spectable_spec_titletext'}):
-                #print "has span"
-                for specs_title_raw in specs_title_child.findChildren('span',
-                                                                      {'class': 'table_spectable_spec_titletext'}):
-                    specs_title_txt = specs_title_raw.text.replace(" ", "").lstrip().rstrip()
-                    spec_title.append(re.sub('\s+', ' ', specs_title_txt))
-            else:
-                #print "no span"
-                specs_title_txt = specs_title_child.text.replace(" ", "").lstrip().rstrip()
-                spec_title.append(re.sub('\s+', ' ', specs_title_txt))
-        for specs_desc_child in item.findChildren('td', {'class': 'table_spectable_specdescription'}):
-            #print specs_desc_child
-            specs_desc_txt = specs_desc_child.text.lstrip().rstrip()
-            spec_desc.append(re.sub('\s+', ' ', specs_desc_txt))
-        #i = 0
-        #while i < len(spec_title):
-        #    print spec_title[i] + ": " + spec_desc[i]
-        #    i += 1
-    for item in soup.findAll('div', {'class': 'product-page'}):
-        print "test: found product-page"
-        for name_child in item.findChildren('span', {'class': 'js-product-name'}):
-            #print "test: found js-product-name"
-            name_raw = name_child.text
-            name = name_raw.strip()
-            print "name: " + name
-        for price_child in item.findChildren('strong', {'class': 'sales-price--current'}):
-            #print price_child
-            price_raw = price_child.text
-            price_array = re.findall(r'\d{1,6}[\,.]{1}\d{0,2}', price_raw)
-            for price_item in price_array:
-                price_str = price_item.replace(',', '.')
-                price_float = float(price_str)
-                price = ('%.2f' % price_float)
-                print "price: " + price
-        for img_child in item.findChildren('img', {'class': 'media-gallery--main-image'}):
-            #print "test: found media-gallery--main-image"
-            source = str(img_child.get('src'))
-            print "source: " + source
-        #print item
-        for specs_title_child in item.findChildren('dt', {'class': 'product-specs--item-title'}):
-            #print specs_title_child
-            if specs_title_child.findChildren('span', {'class': 'product-specs--help-title'}):
-                #print "has span"
-                for specs_title_raw in specs_title_child.findChildren('span', {'class': 'product-specs--help-title'}):
-                    specs_title_txt = specs_title_raw.text.replace(" ", "").lstrip().rstrip()
-                    spec_title.append(re.sub('\s+', ' ', specs_title_txt))
-            else:
-                #print "no span"
-                specs_title_txt = specs_title_child.text.replace(" ", "").lstrip().rstrip()
-                spec_title.append(re.sub('\s+', ' ', specs_title_txt))
-        for specs_desc_child in item.findChildren('dd', {'class': 'product-specs--item-spec'}):
-            #print specs_desc_child
-            if specs_desc_child.findChildren('span', {'class': 'icon-fallback'}):
-                #print "has span"
-                for specs_desc_raw in specs_desc_child.findChildren('span', {'class': 'icon-fallback'}):
-                    specs_desc_txt = specs_desc_raw.text.replace(" ", "").lstrip().rstrip()
-                    spec_desc.append(re.sub('\s+', ' ', specs_desc_txt))
-            else:
-                #print "no span"
-                specs_desc_txt = specs_desc_child.text.replace(" ", "").lstrip().rstrip()
-                spec_desc.append(re.sub('\s+', ' ', specs_desc_txt))
-        #i = 0
-        #while i < len(spec_title):
-        #    print spec_title[i] + ": " + spec_desc[i]
-        #    i += 1
-    manufacture_code = get_manufacture_code(spec_title, spec_desc)
-    print "manufacture_code: " + manufacture_code
-    if not (name == ""):
-        check_for_nodes(cat_name, link_item, name, price, source, date, manufacture_code, spec_title, spec_desc)
+    #print soup
+    for item_container in soup.findAll('div', {'class': 'product_container'}):
+        #print item_container
+        item_name = "product_container"
+    for item_container in soup.findAll('div', {'class': 'product-page'}):
+        #print item_container
+        item_name = "product-page"
+    print "test: found " + item_name
+    name = get_name(item_name, item_container)
+    price = get_price(item_name, item_container)
+    source = get_img(item_name, item_container)
+    spec_title = get_spec_title(item_name, item_container)
+    spec_desc = get_spec_desc(item_name, item_container)
+    manufacture_code = get_manufacture_code(item_name, spec_title, spec_desc)
+    #i = 0
+    #while i < len(spec_title):
+    #    print spec_title[i] + ": " + spec_desc[i]
+    #    i += 1
+    #if not name == "" and manufacture_code == "unknown":
+        #check_for_nodes(cat_name, link_item, name, price, source, date, manufacture_code, spec_title, spec_desc)
         #add_node(cat_name, link_item, name, price, source, date, manufacturer_code, spec_title, spec_desc)
-
-
-def get_manufacture_code(spec_title, spec_desc):
-    index = spec_title.index('Fabrikantcode')
-    print "index: " + str(index)
-    manufacture_code = spec_desc[index]
-    return manufacture_code
 
 
 get_categories()
