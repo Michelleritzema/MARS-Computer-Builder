c26bd3993d41c54e4974862b5e258171ceca1add
Michelleritzema <michelleritzema@hotmail.com>
2014-12-14 17:42:21 +0100

    Play to Laravel Conversion
    Big change: using Laravel instead of Play

---
+++ Icompare/app/views/index.blade.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Made by: Anny & Randa & Michelle
+ * Date: 12/12/2014
+ * Time: 22:15
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="nl">
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Icompare</title>
+    {{ HTML::style('css/bootstrap.min.css'); }}
+    {{ HTML::style('css/main.css'); }}
+    {{ HTML::script('js/jquery-1.11.1.min.js'); }}
+    {{ HTML::script('js/bootstrap.min.js'); }}
+    {{ HTML::script('js/intro_accordion.js'); }}
+</head>
+<body>
+<div class='container'>
+    <div class='banner'>
+        {{ HTML::image('images/banner_blue_shadow.png', 'Icompare banner', array('class' => 'banner_img')) }}
+    </div>
+    <div class= 'sidebar'>
+        <div class='sidebarlinks'>
+            <div class='menu_button'>
+                <a href="#">Button 1</a>
+            </div>
+            <div class='menu_button'>
+                <a href="#">Button 2</a>
+            </div>
+            <div class='menu_button'>
+                <a href="#">Button 3</a>
+            </div>
+        </div>
+    </div>
+    <div class='content'>
+        <div class="intro_accordion">
+            <div class="accordion-section">
+                <a class="accordion-section-title" href="#accordion-1">Meer informatie over deze site
+                    <span class="intro_arrow" href="#accordion-1"></span>
+                    <!--{{ HTML::image('images/arrow_down.png', 'intro arrow', array('id' => 'intro_arrow_span')) }}-->
+                </a>
+                <div id="accordion-1" class="accordion-section-content">
+                    <p>Mauris interdum fringilla augue vitae tincidunt. Curabitur vitae tortor id eros euismod ultrices.
+                        Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent nulla mi,
+                        rutrum ut feugiat at, vestibulum ut neque? Cras tincidunt enim vel aliquet facilisis. Duis congue
+                        ullamcorper vehicula. Proin nunc lacus, semper sit amet elit sit amet, aliquet pulvinar erat.
+                        Nunc pretium quis sapien eu rhoncus. Suspendisse ornare gravida mi, et placerat tellus tempor vitae.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
